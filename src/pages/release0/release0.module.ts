8ff852ded175d734ae020032b88810eba7b83deb
weiwei <827045489qq.com>
2018-04-23 14:40:11 +0800

    new

---
+++ src/pages/release0/release0.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Release0Page } from './release0';
+
+@NgModule({
+  declarations: [
+    Release0Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Release0Page),
+  ],
+})
+export class Release0PageModule {}
